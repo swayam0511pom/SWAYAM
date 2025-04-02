35a778a4354ee6dedcbb6c60afae5fa6c30c6142
swayam0511pom <swayamanchan@gmail.com>
2025-04-02 16:37:47 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/Swayam0511pom/SWAYAM /Save-Restricted-Content-Bot-v2:main
+cd /MissPerfectURLBOT
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
